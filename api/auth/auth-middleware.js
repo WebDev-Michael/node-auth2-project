a5fdfb850abe2f883fa7656cf617aec69d890427
Krygore <webdev.krygore@gmail.com>
2023-05-22 22:00:16 -0700

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,4 +1,6 @@
 const { JWT_SECRET } = require("../secrets"); // use this secret!
+const { findBy } = require('../users/users-model')
+
 
 const restricted = (req, res, next) => {
   /*
@@ -17,6 +19,7 @@ const restricted = (req, res, next) => {
     Put the decoded token in the req object, to make life easier for middlewares downstream!
   */
  next()
+ 
 }
 
 const only = role_name => (req, res, next) => {
@@ -34,7 +37,7 @@ const only = role_name => (req, res, next) => {
 }
 
 
-const checkUsernameExists = (req, res, next) => {
+const checkUsernameExists = async (req, res, next) => {
   /*
     If the username in req.body does NOT exist in the database
     status 401
@@ -42,7 +45,17 @@ const checkUsernameExists = (req, res, next) => {
       "message": "Invalid credentials"
     }
   */
- next()
+    try {
+      const [user] = await findBy({username: req.body.username})
+      if (!user) {
+        next({ status: 401, message: 'Invalid credentials' })
+      } else {
+        req.user = user
+        next()
+      }
+     } catch (err) {
+      next(err)
+     }
 }
 
 
@@ -69,9 +82,10 @@ const validateRoleName = (req, res, next) => {
       req.role_name = 'student'
     } else if (req.body.role_name.trim() === 'admin') {
       next({ status: 422, message: 'Role name can not be admin' })
-    } else if (req.body.role_name.trim().length() > 32) {
+    } else if (req.body.role_name.trim().length > 32) {
       next({ status: 422, message: 'Role name can not be longer than 32 chars' })
     } else {
+      req.role_name = req.body.role_name.trim()
       next()
     }
 }
